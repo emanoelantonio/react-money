1b3b98652ca9252dfce04743f62cea8001de46ad
emanoelantonio <dev.emanoel@protonmail.com>
2023-04-18 10:06:47 -0300

    feat: create Transaction page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { ThemeProvider } from 'styled-components'
-import { Summary } from './components/Summary'
 import { Transactions } from './pages/Transactions'
 import { GlobalStyles } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
@@ -10,7 +9,7 @@ export function App() {
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyles/>
       <Transactions />
-      <Summary/>
+
     </ThemeProvider>
   )
 }
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,39 @@
 import { Header } from '../../components/Header'
+import { Summary } from '../../components/Summary'
+import * as S from './styles'
 
 export const Transactions = () => {
   return (
     <div>
-      <Header/>
+      <Header />
+      <Summary />
+
+      <S.Container>
+        <S.TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento do site</td>
+              <td>
+                <S.PriceHightLight variant='income'>
+                R$ 12.000,00
+                </S.PriceHightLight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburguer</td>
+              <td>
+                <S.PriceHightLight variant='outcome'>
+                - R$ 59,00
+                </S.PriceHightLight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/04/2022</td>
+            </tr>
+          </tbody>
+        </S.TransactionsTable>
+      </S.Container>
     </div>
   )
 }
