4cbacb2f099f83f5fb5c1e0b034bb984ae2b77f3
emanoelantonio <dev.emanoel@protonmail.com>
2023-05-08 16:27:32 -0300

    refactor: implement form validation

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,12 +1,34 @@
+import { zodResolver } from '@hookform/resolvers/zod'
 import { MagnifyingGlass } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
 import * as S from './styles'
 
-export const SearchForm = () => {
-  return (
-    <S.Container>
-      <input type='text' placeholder='Busque por transações' />
+const SearchFormSchema = z.object({
+  query: z.string(),
+})
 
-      <button type='submit'>
+type SearchFormInputs = z.infer<typeof SearchFormSchema>
+
+export const SearchForm = () => {
+
+  const { register, handleSubmit, formState: { isSubmitting } } = useForm<SearchFormInputs>({
+    resolver: zodResolver(SearchFormSchema)
+  })
+
+  function handleSearchTransactions(data: SearchFormInputs) {
+    console.log(data)
+  }
+
+  return (
+    <S.Container onSubmit={handleSubmit(handleSearchTransactions)}>
+      <input
+        type='text'
+        placeholder='Busque por transações'
+        {...register('query')}
+      />
+
+      <button type='submit'disabled={isSubmitting}>
         <MagnifyingGlass size={20} />
         Buscar
       </button>
