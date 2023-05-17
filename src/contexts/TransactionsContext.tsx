22fd4b6b1a031a1860cefac1df551719ad65a70c
emanoelantonio <dev.emanoel@protonmail.com>
2023-05-17 17:00:06 -0300

    Revert "Merge branch 'main' of https://github.com/emanoelantonio/react-money"
    This reverts commit 3942d48290f46b40a0bc2d051143d869e72c09cd, reversing
    changes made to de46bf17f272abec49aeca59973c3e61591bec2f.

---
+++ src/contexts/TransactionsContext.tsx
@@ -0,0 +1,73 @@
+import { ReactNode, useCallback, useEffect, useState } from 'react'
+import { createContext } from 'use-context-selector'
+import { api } from '../lib/axios'
+
+interface TransactionsProps {
+  id: string;
+  description: string;
+  type: 'income' | 'outcome';
+  category: string;
+  price: number;
+  createdAt: string;
+}
+interface CreateTransactionInput {
+  description: string;
+  category: string;
+  price: number;
+  type: 'income' | 'outcome';
+}
+interface TransactionContextType {
+  transactions: TransactionsProps[];
+  fetchTransactions: (query?: string) => Promise<void>;
+  createTransaction: (data: CreateTransactionInput) => Promise<void>
+}
+interface TransactionsProviderProps {
+  children: ReactNode;
+}
+
+
+export const TransactionsContext = createContext({} as TransactionContextType)
+
+export function TransacitonsProvider({ children }: TransactionsProviderProps) {
+  const [transactions, setTransactions] = useState<TransactionsProps[]>([])
+
+  const fetchTransactions = useCallback(async(query?: string) => {
+    const response = await api.get('transactions', {
+      params: {
+        _sort: 'createdAt',
+        _order: 'desc',
+        q: query,
+      },
+    })
+
+    setTransactions(response.data)
+  }, [])
+
+  const createTransaction = useCallback(async(data: CreateTransactionInput) => {
+    const { description, category, price, type } = data
+
+    const response = await api.post('transactions', {
+      description,
+      category,
+      price,
+      type,
+      createdAt: new Date(),
+    })
+
+    setTransactions(state => [response.data,...state])
+  }, [])
+
+  useEffect(() => {
+    fetchTransactions()
+  }, [])
+
+  return (
+    <TransactionsContext.Provider value={{
+      transactions,
+      fetchTransactions,
+      createTransaction,
+    }}>
+      {children}
+    </TransactionsContext.Provider>
+  )
+}
